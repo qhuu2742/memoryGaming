b9b121c655a1398466505949f1e8d5c1ef322bf7
Phạm Quang Hữu <qhuu.official@gmail.com>
2021-10-21 16:09:11 +0700

    add interface

---
+++ main.php
@@ -0,0 +1,73 @@
+<div class="nk-main">
+
+    <div class="nk-gap-2"></div>
+
+
+
+    <div class="container">
+
+        <!-- START: Image Slider -->
+        <?php include "image_slider_main.php" ?>
+        <!-- END: Image Slider -->
+
+        <!-- START: Categories -->
+        <?php include "categories_main.php" ?>
+        <!-- END: Categories -->
+
+        <!-- START: Latest News -->
+        <?php include "latest_news.php" ?>
+        <!-- END: Latest News -->
+
+        <div class="nk-gap-2"></div>
+        <div class="row vertical-gap">
+            <div class="col-lg-8">
+
+                <!-- START: Latest Posts -->
+                <?php include "latest_post.php" ?>
+                <!-- END: Latest Posts -->
+
+                <!-- START: Latest Matches -->
+                <?php include "latest_matches.php" ?>
+                <!-- END: Latest Matches -->
+
+                <!-- START: Tabbed News  -->
+                <?php include "tabbed_news.php" ?>
+                <!-- END: Tabbed News -->
+
+
+                <!-- START: Latest Pictures -->
+                <?php include "latest_pictures.php" ?>
+                <!-- END: Latest Pictures -->
+
+                <!-- START: Best Selling -->
+                <?php include "best_selling.php" ?>
+                <!-- END: Best Selling -->
+            </div>
+            <div class="col-lg-4">
+                <!--
+            START: Sidebar
+
+            Additional Classes:
+                .nk-sidebar-left
+                .nk-sidebar-right
+                .nk-sidebar-sticky
+                -->
+                <?php include "sidebar.php" ?>
+                <!-- END: Sidebar -->
+            </div>
+        </div>
+    </div>
+
+    <div class="nk-gap-4"></div>
+
+
+
+    <!-- START: Footer -->
+    <?php include "footer.php" ?>
+    <!-- END: Footer -->
+
+
+</div>
+
+<img class="nk-page-background-top" src="assets/images/bg-top.png" alt="">
+<img class="nk-page-background-bottom" src="assets/images/bg-bottom.png" alt="">
